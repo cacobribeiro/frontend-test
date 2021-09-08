f6da732c2c32c8be70c9017d2d6797799879ed17
Caio Ribeiro <cacobribeiro@gmail.com>
2021-09-08 16:39:37 -0300

    codigo refatorado e sem bug iniciando ajustes para melhorar o codigo.

--- lib/buildFullUrl.ts
+++ lib/buildFullUrl.ts
@@ -1,73 +1,89 @@
-import kebabCase from 'lodash/kebabCase'
+import kebabCase from 'lodash/kebabCase';
 
-import type { Options } from '../types'
+import type { Options } from '../types';
+
+const notAccept = [NaN, null, undefined];
+
+const regexDoubleSlashes = /(?<=.{8})\/{2,}/g;
+const regexSlashInt = /\/\?/g;
 
 export default function buildFullUrl(opts: Options) {
-  let url: string = opts.baseUrl || ''
-  let endpoint = kebabCase(opts.name)
-  let qs = null
-  if (opts.urlParser) {
-    endpoint = opts.urlParser(opts.name || '')
-  }
-  if (opts.path) {
-    endpoint = opts.path
-  }
-  if (opts.id && !opts.customPath) {
-    endpoint += '/'
-    endpoint += opts.id
-  }
-  if (opts.customPath) {
-    endpoint = opts.customPath
-  }
-  endpoint = endpoint.split('//').join('/')
-  if (endpoint.indexOf('/') == 0) {
-    let arrayEndpoint = endpoint.split('')
-    arrayEndpoint.shift()
-    endpoint = arrayEndpoint.join('')
-  }
-  if (endpoint && url.lastIndexOf('/') != url.length - 1) {
-    url += '/'
-  }
-  url += endpoint
-  if (opts.query) {
-    qs = ''
-    let options = Object.keys(opts.query)
-    for (let i = 0; i < options.length; i++) {
-      let value = opts.query[options[i]]
-      if (value == null || Number.isNaN(value) || typeof value == undefined) {
-        delete opts.query[options[i]]
-      }
+
+    let finalUrl = '';
+
+    if (opts.customPath && opts.id) {
+        opts.customPath = opts.customPath.replace(':id', `${opts.id}`);
     }
-    options = Object.keys(opts.query)
-    for (let i = 0; i < options.length; i++) {
-      let value = opts.query[options[i]]
-      let name = opts.queryStringParser
-        ? opts.queryStringParser(options[i])
-        : options[i]
-      if (
-        (typeof value == 'string' ||
-          typeof value == 'number' ||
-          typeof value == 'boolean') &&
-        !Number.isNaN(value)
-      ) {
-        qs += name + '=' + value
-      } else {
-        value = value as Array<any>
-        for (let u = 0; u < value.length; u++) {
-          qs += name + '[]=' + value[u]
-          if (u < value.length - 1) qs += '&'
+
+    if (opts.baseUrl) {
+        finalUrl = opts.baseUrl || '';
+    }
+
+    if (opts.baseUrl && opts.name) {
+        if (opts.urlParser) {
+            finalUrl = `${opts.baseUrl}/${opts.urlParser(opts.name)}`;
+        } else {
+            finalUrl = `${opts.baseUrl}/${kebabCase(opts.name)}`;
         }
-      }
-      if (i < options.length - 1) qs += '&'
+
+        if (opts.id) finalUrl += `/${opts.id}`;
     }
-  }
-  if (url.lastIndexOf('/') == url.length - 1) {
-    let arrayUrl = url.split('')
-    arrayUrl.pop()
-    url = arrayUrl.join('')
-  }
-  if (qs != null) {
-    url += '?' + qs
-  }
-  return url
+
+    if (opts.customPath) {
+        finalUrl = `${opts.baseUrl}/${opts.customPath}`;
+    }
+
+    if (opts.query && !opts.customPath) {
+        const generatedQuery: string = createQueryString(
+            opts.query,
+            opts.queryStringParser
+        );
+        finalUrl = `${opts.baseUrl}?${generatedQuery}`;
+    }
+
+    if (opts.query && opts.customPath) {
+        const generatedQuery: string = createQueryString(opts.query, null);
+
+        finalUrl = `${opts.baseUrl}${opts.customPath}?${generatedQuery}`;
+    }
+
+    if (finalUrl.lastIndexOf('/') === finalUrl.length - 1)
+        finalUrl = finalUrl.slice(0, finalUrl.length - 2);
+
+    return finalUrl
+        .replace(regexSlashInt, '?')
+        .replace(regexDoubleSlashes, '/');
+}
+
+function createQueryString(query: Options, queryStringParser: any) {
+    const queryList = Object.entries(query).filter(
+        (element: any) => !notAccept.includes(element[1])
+    );
+    return queryList.reduce((acc: string, curr: any, index) => {
+        if (curr[0] === 'tags') {
+            acc += curr[1].reduce(
+                (accumulator: string, current: string, i: number) => {
+                    i >= 1 ? (accumulator += '&') : null;
+                    accumulator += `tags[]=${current}`;
+                    return accumulator;
+                },
+                ''
+            );
+        } else {
+            index >= 1 ? (acc += '&') : null;
+            let key: string = '';
+            let value: string = '';
+
+            if (queryStringParser) {
+                key = queryStringParser(curr[0]);
+                value = queryStringParser(curr[1]);
+            } else {
+                key = kebabCase(curr[0]);
+                value = kebabCase(curr[1]);
+            }
+
+            acc += `${key}=${value}`;
+        }
+        return acc;
+    }, '');
 }
